f0a2a1d18f149ece2e1e6370cd0251f757a4e4c7
smurthy4 <smurthy@verticalrelevance.com>
2022-06-22 15:08:58 -0600

    consumer

--- data_mesh_consumer/data_mesh_consumer_stack.py
+++ data_mesh_consumer/data_mesh_consumer_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_athena as athena,
+    aws_lambda as lambda_,
+    aws_iam as iam
 )
 from constructs import Construct
 
@@ -10,10 +12,29 @@ class DataMeshConsumerStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        #CREATING WORKGROUPS
+        consumer_workgroup = self.create_athena_workgroup(workgroup_name = "consumer")
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "DataMeshConsumerQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        
+        #LAMBDA FUNCTION FOR RESOURCE LINK
+        fn = lambda_.Function(self, "lf-resource-link",
+                              runtime = lambda_.Runtime.PYTHON_3_8,
+                              handler = "create_resource_link.lambda_handler",
+                              code = lambda_.Code.from_asset("resource_link_lambda"))
+        
+        fn.role.add_managed_policy(policy = iam.ManagedPolicy.from_managed_policy_arn(self, "Lambda Glue Access", 
+                                                                                      managed_policy_arn = "arn:aws:iam::aws:policy/AWSGlueConsoleFullAccess"))
+        
+
+
+    
+    #FUNCTION TO CREATE WORK GROUP
+
+    def create_athena_workgroup(self, workgroup_name: str):
+    
+        workgroup = athena.CfnWorkGroup(self, workgroup_name,
+                                        name = workgroup_name,
+                                        state = "ENABLED")
+
+        return workgroup
+
